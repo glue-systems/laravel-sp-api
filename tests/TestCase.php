430f750aed2c905c186980c8641d134d37b2046b
John Silkey Jr <26827190+jpsilkjr86@users.noreply.github.com>
2023-09-06 01:22:30 +0000

    Adding comments

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,6 +6,10 @@ use Illuminate\Contracts\Console\Kernel;
 use Illuminate\Foundation\Bootstrap\LoadEnvironmentVariables;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 
+/**
+ * Following setup instructions from orchestral/testbench docs:
+ * https://github.com/orchestral/testbench/tree/476ef1adabbddbf5ba72705590ee67737a86decc
+ */
 class TestCase extends BaseTestCase
 {
     protected function getPackageProviders($app)
@@ -30,6 +34,11 @@ class TestCase extends BaseTestCase
      */
     protected function getEnvironmentSetUp($app)
     {
+        // There was no documentation for v3 that I could find on how to
+        // implement hydrating of a config file that relies on env values
+        // (standard Laravel stuff). The below implementation follows ideas from
+        // https://github.com/orchestral/testbench/issues/211#issuecomment-360885812
+        // as well as what I could figure out from the testbench source code.
         $app->useEnvironmentPath(__DIR__ . '/..');
 
         $app->afterBootstrapping(LoadEnvironmentVariables::class, function ($app) {
