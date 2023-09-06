7c382d1cd8b8887855866b2fca14325d9f69feb4
John Silkey Jr <26827190+jpsilkjr86@users.noreply.github.com>
2023-09-06 15:36:29 +0000

    Removing check for static::isMock before calling clearResolvedInstance, as it is unnecessary given that Mockery already retains static references to its mocks

--- src/Traits/RefreshesFacadeInstance.php
+++ src/Traits/RefreshesFacadeInstance.php
@@ -11,9 +11,14 @@ trait RefreshesFacadeInstance
      */
     public static function getFacadeRoot()
     {
-        if (!static::isMock()) {
-            static::clearResolvedInstance(static::getFacadeAccessor());
-        }
+        // Note that this only clears the Facade's internal static reference
+        // to the resolved instance. Depending on the implementation of the
+        // binding, it is still possible for the same instance to be returned
+        // on subsequent calls to resolveFacadeInstance, such as when the
+        // object is registered as a singleton in a Service Provider, or when a
+        // testing helper like `[Facade]::shouldReceive` is used, as the Mockery
+        // library also retains static references to its mocks.
+        static::clearResolvedInstance(static::getFacadeAccessor());
 
         return static::resolveFacadeInstance(static::getFacadeAccessor());
     }
--- tests/Facades/SpApiFacadeTest.php
+++ tests/Facades/SpApiFacadeTest.php
@@ -3,16 +3,25 @@
 namespace Tests\Facades;
 
 use Glue\SpApi\Laravel\Facades\SpApi;
-use Glue\SpApi\OpenAPI\Clients\TokensV20210301\Model\CreateRestrictedDataTokenRequest;
 use Tests\TestCase;
 
 class SpApiFacadeTest extends TestCase
 {
-    public function test_it_uses_new_instances_on_each_static_call()
+    public function test_it_instantiates_new_facade_roots_on_each_static_call()
     {
-        $spApi1 = SpApi::withRdtRequest(new CreateRestrictedDataTokenRequest());
-        $spApi2 = SpApi::withRdtRequest(new CreateRestrictedDataTokenRequest());
+        $spApi1 = SpApi::getFacadeRoot();
+        $spApi2 = SpApi::getFacadeRoot();
 
         $this->assertNotSame($spApi1, $spApi2);
+    }
+
+    public function test_it_uses_same_instance_when_facade_is_mocked()
+    {
+        SpApi::shouldReceive()->zeroOrMoreTimes();
+        $spApi1 = SpApi::getFacadeRoot();
+        SpApi::shouldReceive()->zeroOrMoreTimes();
+        $spApi2 = SpApi::getFacadeRoot();
+
+        $this->assertSame($spApi1, $spApi2);
     }
 }
